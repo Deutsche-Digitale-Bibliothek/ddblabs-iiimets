ec770d7854e968f7a05f17ad6f63ff0b2e57e803
Karl <kraegelin@sub.uni-goettingen.de>
2021-12-17 13:16:29 +0100

    first commit

---
+++ iiif_conversion.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# iiif_conversion.py
+# @Author : Karl Kraegelin (karlkraegelin@outlook.com)
+# @Link   : 
+# @Date   : 17.12.2021, 13:01:16
+
+import json
+import yaml
+import requests
---
+++ iiif_harvesting.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# iiif_harvesting.py
+# @Author : Karl Kraegelin (karlkraegelin@outlook.com)
+# @Link   : 
+# @Date   : 17.12.2021, 13:01:27
