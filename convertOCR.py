99909632d36f603ae243e83db2953618fd29ec6e
Karl Krägelin <kraegelin@sub.uni-goettingen.de>
2022-01-19 10:38:49 +0100

    Update

--- convertOCR.py
+++ convertOCR.py
@@ -4,8 +4,10 @@ sys.path.append('/home/cloud/python-saxon')
 import saxonc
 import requests
 import re
+import os
 
-def transformHOCR(urls, folder):
+def transformHOCR(urls, folder, logger):
+    print("Transform hOCR")
     with saxonc.PySaxonProcessor(license=False) as proc:
         xdmAtomicval = proc.make_boolean_value(False)
         xsltproc = proc.new_xslt_processor()
@@ -13,14 +15,24 @@ def transformHOCR(urls, folder):
 
         for u in urls:
             filename = re.sub(r'https://api.digitale-sammlungen.de/ocr/(.+)/(.+)', r'\1_\2.xml', u)
-            r = requests.get(u)
-            r.encoding = 'utf-8'
-            document = proc.parse_xml(xml_text=r.text)
-            xsltproc.set_source(xdm_node=document)
-            result = xsltproc.transform_to_string()
-            with open(os.path.join(folder, filename), 'w') as f:
-                f.write(result)
-                print(f"Wrote {filename}")
+            try:
+                r = requests.get(u)
+            except:
+                logger.error(f"Konnte {u} nicht abrufen")
+                pass
+            else:
+                r.encoding = 'utf-8'
+                try:
+                    document = proc.parse_xml(xml_text=r.text)
+                    xsltproc.set_source(xdm_node=document)
+                    result = xsltproc.transform_to_string()
+                except:
+                    logger.error(f"Konnte {u} nicht transformieren")
+                    pass
+                else:
+                    with open(os.path.join(folder, filename), 'w') as f:
+                        f.write(result)
+                        print(f"Wrote {filename}")
 
 if __name__ == '__main__':
     urls = ['https://api.digitale-sammlungen.de/ocr/bsb00063967/740', 'https://api.digitale-sammlungen.de/ocr/bsb00063967/739']
