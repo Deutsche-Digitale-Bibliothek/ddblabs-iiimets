ec9235271c04c1e7bf7969da981e876978a8c8f2
Karl Krägelin <kraegelin@sub.uni-goettingen.de>
2022-03-01 13:14:01 +0100

    Benutzung von requests_futures

--- src/download_ocrxml.py
+++ src/download_ocrxml.py
@@ -10,7 +10,10 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 import requests
 import asyncio
+from threading import Lock
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+from requests_futures.sessions import FuturesSession
 from loguru import logger
 import os
 import subprocess
@@ -21,80 +24,7 @@ import time
 import re
 from pathlib import Path
 
-START_TIME = default_timer()
 
-def fetch(session, url, outfolder):
-
-    with session.get(url) as response:
-        response.encoding = 'utf-8'
-        data = response.text
-        if response.status_code != 200:
-            logger.critical(f"Statuscode {response.status_code} bei {url}")
-        else:
-            filename = re.sub(r'https://api.digitale-sammlungen.de/ocr/(.+)/(.+)', r'\1_\2.xml', url)
-            with open(os.path.join(outfolder, filename), "w", encoding="utf8") as of:
-                of.write(data)
-        return url
-
-async def get_data_asynchronous(alto_urls, outfolder):
-
-    with ThreadPoolExecutor(max_workers=16) as executor:
-        with requests.Session() as session:
-            retry_strategy = Retry(
-                total=6,
-                status_forcelist=[429, 500, 502, 503, 504],
-                allowed_methods=["GET"],
-                backoff_factor=1
-            )
-            adapter = HTTPAdapter(max_retries=retry_strategy)
-            session.mount("https://", adapter)
-            session.mount("http://", adapter)
-            loop = asyncio.get_event_loop()
-            START_TIME = default_timer()
-            tasks = [
-                loop.run_in_executor(
-                    executor,
-                    fetch,
-                    *(session, url, outfolder) # Allows us to pass in multiple arguments to `fetch`
-                )
-                for url in alto_urls
-            ]
-
-def main(alto_urls, outfolder):
-    loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(get_data_asynchronous(alto_urls, outfolder))
-    loop.run_until_complete(future)
-    logger.debug(f"Vergangene Zeit für den OCR Abruf: {round((default_timer() - START_TIME) / 60, 2)} Minuten")
-
-
-def downloadhOCR(metsfolder, hocrfolder):
-
-    namespaces = {"alto": "http://www.loc.gov/standards/alto/ns-v3#", "xlink": "http://www.w3.org/1999/xlink",
-                "mets": "http://www.loc.gov/METS/", "mods": "http://www.loc.gov/mods/v3"}
-
-    files = [os.path.join(metsfolder, x) for x in os.listdir(metsfolder) if x.endswith('xml')]
-
-    alto_urls = []
-    logger.info(f"Parse {len(files)} Dateien nach Volltext Links")
-    for f in files:
-        tree = ET.parse(bytes(f,encoding='utf8'))
-        fulltextMets_file = tree.findall(f".//mets:fileGrp[@USE='FULLTEXT']/mets:file[@MIMETYPE = 'text/xml']", namespaces)
-        for e in fulltextMets_file:
-            for flocat in e:
-                altourl = flocat.attrib['{http://www.w3.org/1999/xlink}href']
-                alto_urls.append(altourl)
-
-    logger.info(f"Starte Download von {len(alto_urls)} urls")
-
-    main(alto_urls, hocrfolder)
-    logger.info(f"Download der hOCR Dateien erledigt.")
-    # Change Links
-    for f in files:
-        with open(f, 'r+', encoding='utf8') as fl:
-            cont = fl.read()
-            cont = re.sub(r'https://api.digitale-sammlungen.de/ocr/(.+?)/(.+)"', r'\1_\2.xml"', cont)
-            fl.write(cont)
-    logger.info(f"Anpassen der Links in der fileGroup FULLTEXT erfolgt.")
 
 def runXSLonFolder(hocrfolder, altofolder, cwd, saxonpath):
 
@@ -108,3 +38,78 @@ def runXSLonFolder(hocrfolder, altofolder, cwd, saxonpath):
         if len(re.findall(r'\d+\stransformations failed', transformationoutput)) == 1:
             logger.warning(re.findall(r'\d+\stransformations failed', transformationoutput)[0])
 
+
+
+
+def setup_requests() -> requests.Session:
+        """Sets up a requests session to automatically retry on errors
+
+        cf. <https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/>
+
+        Returns
+        -------
+        http : requests.Session
+            A fully configured requests Session object
+        """
+        http = requests.Session()
+        assert_status_hook = (
+            lambda response, *args, **kwargs: response.raise_for_status()
+        )
+        http.hooks["response"] = [assert_status_hook]
+        retry_strategy = Retry(
+            total=3,
+            status_forcelist=[429, 500, 502, 503, 504],
+            allowed_methods=["GET"],
+            backoff_factor=1,
+        )
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        http.mount("https://", adapter)
+        http.mount("http://", adapter)
+        return http
+
+START_TIME = default_timer()
+
+metsfolder = "/home/cloud/storage/iiimets/_METS/2022-02-28"
+outfolder = "/home/cloud/storage/iiimets/_OCR/2022-02-28/hOCR"
+namespaces = {"alto": "http://www.loc.gov/standards/alto/ns-v3#", "xlink": "http://www.w3.org/1999/xlink",
+            "mets": "http://www.loc.gov/METS/", "mods": "http://www.loc.gov/mods/v3"}
+
+files = [os.path.join(metsfolder, x) for x in os.listdir(metsfolder) if x.endswith('xml')]
+
+alto_urls = []
+logger.info(f"Parse {len(files)} Dateien nach Volltext Links")
+for f in files[:100]:
+    tree = ET.parse(bytes(f,encoding='utf8'))
+    fulltextMets_file = tree.findall(f".//mets:fileGrp[@USE='FULLTEXT']/mets:file[@MIMETYPE = 'text/xml']", namespaces)
+    for e in fulltextMets_file:
+        for flocat in e:
+            altourl = flocat.attrib['{http://www.w3.org/1999/xlink}href']
+            alto_urls.append(altourl)
+
+logger.info(f"Starte Download von {len(alto_urls)} urls")
+
+with FuturesSession(max_workers=16) as session:
+    futures = [session.get(url) for url in alto_urls]
+    for future in as_completed(futures):
+        response = future.result()
+        url = response.request.url
+        print(url)
+        response.encoding = 'utf-8'
+        data = response.text
+        if response.status_code != 200:
+            logger.critical(f"Statuscode {response.status_code} bei {future}")
+        else:
+            filename = re.sub(r'https://api.digitale-sammlungen.de/ocr/(.+)/(.+)', r'\1_\2.xml', url)
+            with open(os.path.join(outfolder, filename), "w", encoding="utf8") as of:
+                of.write(data)
+
+
+logger.debug(f"Vergangene Zeit für den OCR Abruf: {round((default_timer() - START_TIME) / 60, 2)} Minuten")
+
+# Change Links
+# for f in files:
+#     with open(f, 'r+', encoding='utf8') as fl:
+#         cont = fl.read()
+#         cont = re.sub(r'https://api.digitale-sammlungen.de/ocr/(.+?)/(.+)"', r'\1_\2.xml"', cont)
+#         fl.write(cont)
+# logger.info(f"Anpassen der Links in der fileGroup FULLTEXT erfolgt.")
