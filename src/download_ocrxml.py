8ee7e57975f521307862607b96f140fb098c0edf
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2022-03-30 12:56:44 +0200

    fix automerge (duplicate code)

--- src/download_ocrxml.py
+++ src/download_ocrxml.py
@@ -106,45 +106,6 @@ def downloadhOCR(metsfolder="_METS/2022-02-28", outfolder="_OCR/2022-02-28/hOCR"
 
     logger.debug(f"Vergangene Zeit für den OCR Abruf: {round((default_timer() - start_time) / 60, 2)} Minuten")
 
-START_TIME = default_timer()
-
-metsfolder = "./_METS/2022-02-28"
-outfolder = "./_OCR/2022-02-28/hOCR"
-namespaces = {"alto": "http://www.loc.gov/standards/alto/ns-v3#", "xlink": "http://www.w3.org/1999/xlink",
-            "mets": "http://www.loc.gov/METS/", "mods": "http://www.loc.gov/mods/v3"}
-
-files = [os.path.join(metsfolder, x) for x in os.listdir(metsfolder) if x.endswith('xml')]
-
-alto_urls = []
-logger.info(f"Parse {len(files)} Dateien nach Volltext Links")
-for f in files[:100]:
-    tree = ET.parse(bytes(f,encoding='utf8'))
-    fulltextMets_file = tree.findall(f".//mets:fileGrp[@USE='FULLTEXT']/mets:file[@MIMETYPE = 'text/xml']", namespaces)
-    for e in fulltextMets_file:
-        for flocat in e:
-            altourl = flocat.attrib['{http://www.w3.org/1999/xlink}href']
-            alto_urls.append(altourl)
-
-logger.info(f"Starte Download von {len(alto_urls)} urls")
-
-with FuturesSession(max_workers=8) as session:
-    futures = [session.get(url, headers={'User-agent': 'iiimets'}) for url in alto_urls]
-    for future in as_completed(futures):
-        response = future.result()
-        url = response.request.url
-        response.encoding = 'utf-8'
-        data = response.text
-        if response.status_code != 200:
-            # print(response.headers)
-            logger.critical(f"Statuscode {response.status_code} bei {url}")
-        else:
-            filename = re.sub(r'https://api.digitale-sammlungen.de/ocr/(.+)/(.+)', r'\1_\2.xml', url)
-            with open(os.path.join(outfolder, filename), "w", encoding="utf8") as of:
-                of.write(data)
-
-
-logger.debug(f"Vergangene Zeit für den OCR Abruf: {round((default_timer() - START_TIME) / 60, 2)} Minuten"
-
 # Change Links
 # for f in files:
 #     with open(f, 'r+', encoding='utf8') as fl:
