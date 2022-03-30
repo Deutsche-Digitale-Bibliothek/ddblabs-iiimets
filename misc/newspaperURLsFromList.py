e0ecf64831523b46173fb21110e95c2540bed70d
Karl Krägelin <kraegelin@sub.uni-goettingen.de>
2022-03-30 08:51:24 +0000

    more cleaning

--- misc/newspaperURLsFromList.py
+++ misc/newspaperURLsFromList.py
@@ -1,9 +1,9 @@
 # %%
 import pickle
 
-inf = 'bsb_newspapers_01.txt'
+inf = 'periodikaausgaben_bsb_20211220.txt'
 m = ['https://api.digitale-sammlungen.de/iiif/presentation/v2/' + line.rstrip('\n') + '/manifest' for line in open(inf)]
 
-with open('bsb_newspapers_01.pkl', 'wb') as f:
+with open('periodikaausgaben_bsb_20211220.txt.pkl', 'wb') as f:
     pickle.dump(m, f)
 # %%
