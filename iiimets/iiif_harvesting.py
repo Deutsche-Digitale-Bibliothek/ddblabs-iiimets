601e47febb6cf69a04958c255bdf0836d519ea13
karl <karlbecker87@gmail.com>
2022-06-08 12:29:49 +0200

    improving the harvesting process

--- iiimets/iiif_harvesting.py
+++ iiimets/iiif_harvesting.py
@@ -10,19 +10,20 @@ import sys
 import pickle
 import time
 from pathlib import Path
+from urllib import request
 import pandas as pd
+import requests
 
-'''
-- Get first (https://api.digitale-sammlungen.de/iiif/presentation/v2/collection/top?cursor=initial)
-- for m in /manifests: Log @id
-- work on /next: https://api.digitale-sammlungen.de/iiif/presentation/v2/collection/top?cursor=AoIIP4AAACtic2IxMTczMzAwMg==
-- total number is available
-- return list with Manifest URLs
-'''
 
-def getIdentifier(url, session, logger):
 
-    # Die Liste nutzen wir nur zum Anzeigen des Fortschritts.
+def getIdentifier(url: str, session: requests.Session, logger) -> list:
+    '''
+    - Get first (https://api.digitale-sammlungen.de/iiif/presentation/v2/collection/top?cursor=initial)
+    - for m in /manifests: Log @id
+    - work on /next: https://api.digitale-sammlungen.de/iiif/presentation/v2/collection/top?cursor=AoIIP4AAACtic2IxMTczMzAwMg==
+    - total number is available
+    - return list with Manifest URLs
+    '''
     manifests = []
 
     def getManifestURLs(response):
@@ -33,16 +34,19 @@ def getIdentifier(url, session, logger):
                 d['url'] = i['@id']
                 d['name'] = i['label']
                 manifests.append(d)
-            print(len(manifests))
+
     try:
-        response = session.get(url,
-                                        verify=False,
-                                        timeout=(20, 80))
+        response = session.get(url, verify=False, timeout=(20, 80))
     except Exception as e:
         logger.error(f'The collection URL is not reachable: {e}')
         sys.exit()
     else:
-        print(f'total number of Manifests: {response.json()["total"]}')
+        try:
+            response.json()["total"]
+        except:
+            pass
+        else:
+            print(f'Total number of Manifests: {response.json()["total"]}')
 
         # Jetzt kommt die eigentliche Schleife: Solange wir da nicht per break rausgehen, läuft die.
         # Immer mit neuer URL
@@ -50,7 +54,6 @@ def getIdentifier(url, session, logger):
             # time.sleep(0.5)
             # Verbindungsversuch inkl. Errorhandling
             try:
-                print(url)
                 response = session.get(url,
                                         verify=False,
                                         timeout=(20, 80))
@@ -68,35 +71,36 @@ def getIdentifier(url, session, logger):
                     try:
                         # schauen, ob es noch einen Token gibt oder ob wir aus der Schleife rausmüssen:
                         url = response.json()["next"]
+                        # This is for testing purposes
                         # if len(manifests) > 300:
                         #     break
                     except:
                         # wir sind fertig und gehen per break aus dem while-Loop raus.
-                        print(f"Identifier Harvesting beendet. Insgesamt {len(manifests)} IDs bekommen.")
+                        logger.info(f"Identifier Harvesting beendet. Insgesamt {len(manifests)} IDs bekommen.")
                         break
+        # manifests is a list of dictionaries with keys `url` and `name` of resource
         return manifests
 
-def getNewspaperManifests(url, session, filter: str, cwd, logger):
+def getListOfManifests(url: str, session: requests.Session, filter: str, cwd: Path, logger) -> list:
+    '''
+    - Retrieve Manifest URLs form IIIF Collection URL
+    - Filter the Manifests by title if filter is passes
+    - Pickle results
+    - Return a List of Manifest URLs
+    '''
     manifests = getIdentifier(url, session, logger)
     df = pd.DataFrame.from_records(manifests)
     df.to_pickle('allmanifests.pkl')
-    # newspaper_urls = df.query('name.str.contains("##")', engine="python")['url'].to_list()
 
     if filter is not None:
         logger.info(f"Filter Manifests by {filter}")
-        newspaper_urls = df.query(f'name.str.contains("{filter}")', engine="python")['url'].to_list()
+        manifest_urls = df.query(f'name.str.contains("{filter}")', engine="python")['url'].to_list()
     else:
-        newspaper_urls = df['url'].to_list()
+        manifest_urls = df['url'].to_list()
 
-    with open(Path(cwd, 'cache', 'newspaper_urls.pkl'), 'wb') as f:
-        pickle.dump(newspaper_urls, f)
-    return newspaper_urls
-
-def generateNewspaperURLfromFile(fpath):
-    import random
-    newspaper_urls = ['https://api.digitale-sammlungen.de/iiif/presentation/v2/' + line.rstrip('\n') + '/manifest' for line in open(fpath)]
-    with open('newspaper_urls.pkl', 'wb') as f:
-        pickle.dump(random.sample(newspaper_urls, int(len(newspaper_urls)/1000)), f)
+    with open(Path(cwd, 'cache', 'manifest_urls.pkl'), 'wb') as f:
+        pickle.dump(manifest_urls, f)
+    return manifest_urls
 
 if __name__ == '__main__':
-    generateNewspaperURLfromFile('periodikaausgaben_bsb_20211220.txt')
+     getListOfManifests(url, http, filter, cwd, logger)
